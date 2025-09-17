2c777355eac71ea36cadfec20bbbb319722ffa76
alraya <vvocloner@gmail.com>
2025-09-17 04:31:35 +0300

    Auto backup at Wed 09/17/2025  4:28:10.53

--- platform/src/routes/api.js
+++
@@ -1,9 +0,0 @@
-// src/routes/api.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/hello', (req, res) => {
-  res.json({ ok: true, msg: 'Hello from platform API' });
-});
-
-module.exports = router;
